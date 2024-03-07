ebca9c571c596f7bd23c27062fa9a9a461cfd3ef
Charlene Hawi <charlenehawi16@gmail.com>
2024-03-07 16:28:22 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+
+# What is the user's name?
+name = input("Enter your name: ")
+
+# What is the user's age?
+age = input("Enter your age: ")
+
+# What is the user's location?
+location = input("Enter your location: ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
